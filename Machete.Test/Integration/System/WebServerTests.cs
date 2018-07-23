0b40b2c13bc13d0a61c688f522795d863bdd2847
jcii <jciispam@gmail.com>
2018-07-22 22:51:17 -0700

    Add initialization tests for API and IDP server

--- Machete.Test/Integration/System/WebServerTests.cs
+++ Machete.Test/Integration/System/WebServerTests.cs
@@ -18,20 +18,26 @@ namespace Machete.Test.Integration.System
         //private sharedUI ui;
         private FluentRecordBase frb;
         //private static IMapper map;
-        private static IISExpress iis;
+        private static IISExpress web;
+        private static IISExpress api;
+        private static IISExpress idp;
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
-            iis = new IISExpress("Machete.Web", "4213");
-            iis.StartIis();
+            web = new IISExpress("Machete.Web", "4213");
+            api = new IISExpress("Api", "63374");
+            idp = new IISExpress("Identity", "63273");
+            web.StartIis();
+            api.StartIis();
+            idp.StartIis();
         }
 
         [TestInitialize]
         public void TestInitialize() { }
 
         [TestMethod, TestCategory(TC.E2E), TestCategory(TC.Controller), TestCategory(TC.TestHarness)]
-        public async Task E2E_WebServer_Starts()
+        public async Task E2E_MacheteWeb_Starts()
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://localhost:4213");
@@ -42,7 +48,35 @@ namespace Machete.Test.Integration.System
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
+        [TestMethod, TestCategory(TC.E2E), TestCategory(TC.Controller), TestCategory(TC.TestHarness)]
+        public async Task E2E_API_Starts()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:63374");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetAsync("/api/configs");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod, TestCategory(TC.E2E), TestCategory(TC.Controller), TestCategory(TC.TestHarness)]
+        public async Task E2E_IDP_Starts()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44379");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetAsync("/id/.well-known/openid-configuration");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
         [ClassCleanup]
-        public static void ClassCleanup() { iis.StopIis(); }
+        public static void ClassCleanup() {
+            web.StopIis();
+            api.StopIis();
+            idp.StopIis();
+        }
     }
 }
